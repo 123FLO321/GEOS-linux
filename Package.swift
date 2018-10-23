dfcfacb9a3415f92e5071dd841628eeb65c6825e
Florian Kostenzer <florian.kostenzer@icloud.com>
2018-10-23 14:41:09 +0200

    added geos module

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:4.1
+import PackageDescription
+
+let package = Package(
+    name: "GEOS",
+    products: [
+        .library(name: "GEOS", targets: ["GEOS"])
+    ],
+    dependencies: [],
+    targets: []
+)
